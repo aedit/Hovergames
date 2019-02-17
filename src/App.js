38a7f42829cfc3871ffc49751fe39b8e2bdabf44
vaibhav bhawalkar <vaibhavbhawalkar98.vb@gmail.com>
2019-02-17 17:16:55 +0530

    romoved additional boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import styled from "styled-components"
+import posed from "react-pose"
+import "./App.css"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const W = posed.div({
+  visible: {
+    scale: 1,
+  },
+  hidden: {
+    scale: 0,
+  },
+})
+
+const Window = styled(W)`
+  height: 90vh;
+  width: 95vw;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  background: black;
+  box-shadow: 0 0 100px rgba(255, 255, 255, 0.4);
+`
+
+const App = () => {
+  const windowPose = useWindowPose(1.5)
+  return (
+    <Window pose={windowPose}>
+      <h1>Hello, World!</h1>
+    </Window>
+  )
 }
 
-export default App;
+const useWindowPose = timeOut => {
+  const [windowPose, setWindowPose] = React.useState("hidden")
+  React.useEffect(
+    () => void setTimeout(setWindowPose("visible"), timeOut * 1000),
+    [],
+  )
+  return windowPose
+}
+
+export default App
