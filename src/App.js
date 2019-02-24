0813d9e59f750dcf739fdaccbad007c9070b2127
vaibhav bhawalkar <vaibhavbhawalkar98.vb@gmail.com>
2019-02-24 13:48:16 +0530

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import React from "react";
-import GestureList from "./components/GestureList";
-import "./App.css";
-import { useFromToPose, Window } from "./ui-components";
+import React from 'react'
+import './App.css'
+
+import SplitText from 'react-pose-text'
+import { useFromToPose, Window, Heading, Subtitle } from './ui-components'
+
+// const gestures = [
+//   { id: 1, name: 'OPEN' },
+//   { id: 2, name: 'CLOSE' },
+//   { id: 3, name: 'MOVE' },
+//   { id: 4, name: 'DRAG' },
+//   { id: 5, name: 'CLICK' },
+//   { id: 6, name: 'HOLD' },
+//   { id: 7, name: 'SCROLL' },
+//   { id: 8, name: 'PULLDOWN' },
+//   { id: 9, name: 'BACK' },
+// ]
+
+const charPose = {
+  enter: {
+    scale: 1,
+  },
+  exit: {
+    scale: 0,
+  },
+}
 
 const App = () => {
-  const windowPose = useFromToPose(1.5, { from: "hidden", to: "visible" });
+  const windowPose = useFromToPose(1.5, { from: 'hidden', to: 'visible' })
+  const charPoses = useFromToPose(10, { from: 'exit', to: 'enter' })
   return (
     <Window pose={windowPose}>
-      <h1>
-        Games On Finger || Finger on games || GamESTURE{" "}
-        <span>something about "NAME"...</span>
-      </h1>
-      <h2>Instructions</h2>
-      <GestureList
-        gestures={[{ id: 1, name: "OPEN" }, { id: 2, name: "CLOSE" }]}
-      />
+      <Heading>
+        <SplitText charPoses={charPose} pose={charPoses}>
+          Hover
+        </SplitText>
+        <Subtitle>something about "NAME"...</Subtitle>
+      </Heading>
     </Window>
-  );
-};
+  )
+}
 
-export default App;
+export default App
