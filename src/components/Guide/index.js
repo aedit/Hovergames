fe668eafba417748e40e89528b9c88e2f9fca356
aedit <udisen97@gmail.com>
2019-03-13 19:08:57 +0530

    Updated Guide

--- src/components/Guide/index.js
+++ src/components/Guide/index.js
@@ -6,13 +6,21 @@ const GuideWindow = styled(Window)`
     grid-template-areas:
         'heading'
         'desc'
+        'empty'
         'instruction'
         'instruction';
-    grid-template-rows: 1fr 1fr 3fr;
+    grid-template-rows: 1fr 1fr 50px 2fr 2fr;
     justify-items:center;
+    overflow:hidden;
 `
 const Instruction = styled.section`
     grid-area:instruction;
+    display:flex;
+    flex-wrap:wrap;
+    & article {
+        flex-basis:50%;
+        text-align:center;
+    }
 `
 
 const Desc = styled.article`
@@ -30,6 +38,7 @@ const Guide = () => {
         <Desc>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quisquam corporis eius corrupti eligendi officiis esse iure provident repellat tempore non fugit obcaecati voluptates ipsum facere nemo possimus illo veritatis beatae, totam necessitatibus? Nemo iusto dolorum odit, ducimus velit optio inventore placeat eos, aliquid ut, modi provident laboriosam eveniet corporis.
         </Desc>
+        <Instruction></Instruction>
     </GuideWindow>
   )
 }
