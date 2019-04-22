2122a32e7054d9b97a03757506e793d3dd4fd610
aedit <udisen97@gmail.com>
2019-04-22 20:40:31 +0530

    About updated

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -28,24 +28,27 @@ const About = ({ informUp }) => {
     >
       <Heading style={{ gridArea: 'heading' }}>About Page</Heading>
       <Desc>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-        aperiam repellendus facere saepe aliquam quidem, nulla voluptas suscipit
-        reprehenderit, esse autem minus atque eligendi necessitatibus. Saepe sit
-        laboriosam, obcaecati minima excepturi quasi porro quidem ipsum sint
-        natus pariatur similique cum eius! Deserunt impedit cumque
-        necessitatibus recusandae aut eaque, eligendi architecto?Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Ullam placeat a ipsum
-        delectus, voluptatem deserunt eligendi praesentium neque beatae nihil
-        provident autem quam deleniti quasi consectetur fugiat animi incidunt
-        porro inventore voluptates dignissimos obcaecati atque. Accusantium
-        ratione vel officia fugit maiores harum perferendis? Numquam pariatur
-        illo reprehenderit aspernatur, similique voluptate doloremque et
-        possimus doloribus ad voluptates qui saepe illum perspiciatis libero
-        quia aperiam iste voluptatibus recusandae eius vero aliquam dolor. Odit,
-        soluta. Error sed minus laboriosam quam quos tempore impedit. Pariatur,
-        laudantium suscipit ad repellat adipisci dolorum in dolorem consequatur
-        ducimus quae asperiores similique molestiae sint ab excepturi eum!
-        Possimus?
+        HOVER Games is a Unique Game Playing platform where game enthusiasts are
+        provided with a new feature of experienceing the gaming world. HOVER
+        Games is completely designed to respond based only on your hand
+        gestures. At HOVER Games, you wouldnt need to use your keyboard or
+        mouse, for anything. With game playing, the whole platform responses to
+        player's gestures as well. It currently supports eight uniquley
+        identified hand moments viz.
+        <ol style={{ padding: '2em' }}>
+          <li>Up Gesture</li>
+          <li>Down Gesture</li>
+          <li>Left Gesture</li>
+          <li>Right Gesture</li>
+          <li>Open Gesture</li>
+          <li>Close Gesture</li>
+          <li>Horizontal Motion</li>
+          <li>Vertical Motion</li>
+        </ol>
+        <h5>You have a whole new world in your palm.</h5>
+        <strong style={{ textAlign: 'right', width: '100%' }}>
+          More Updates Coming Soon
+        </strong>
       </Desc>
     </UD>
   )
