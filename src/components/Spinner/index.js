e866648e9f611484966f8084addd644dcfa4b33b
vbhv98 <vaibhavbhawalkar98.vb@gmail.com>
2019-04-23 15:03:29 +0530

    spinner final

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import LoadImg from '../../designs/LoadImg.webp'
 
 const Spin = styled.div`
   position: absolute;
@@ -7,37 +8,18 @@ const Spin = styled.div`
   bottom: 0;
   right: 0;
   left: 0;
-  background: #0f0c29;
+  background: #fff;
   display: flex;
   justify-content: center;
   align-items: center;
   z-index: 1000;
-  span {
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-    border: 5px solid rgba(0, 0, 0, 0);
-    border-top-color: #7afdd6;
-    border-bottom-color: #7afdd6;
-    animation: spin 1s linear infinite;
-  }
-  @keyframes spin {
-    from {
-      transform: rotate(0deg) scale(1);
-    }
-    50% {
-      transform: rotate(180deg) scale(1.2);
-    }
-    to {
-      transform: rotate(360deg) scale(1);
-    }
-  }
 `
 
 const Spinner = () => {
   return (
     <Spin>
-      <span />
+      {/* <span /> */}
+      <img src={LoadImg} alt="Loader" />
     </Spin>
   )
 }
